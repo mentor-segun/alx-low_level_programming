58349bd4a2a37c92c44947465d2b93862a52d285
Segun Aremu <segun.aremu@outlook.com>
2023-10-05 16:29:24 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
  * main - entry point
- * 
- *This prints a statement with printf.
- *
+ * This prints a statement with printf.
  * return: always 0 (success)
  */
 int main(void)
