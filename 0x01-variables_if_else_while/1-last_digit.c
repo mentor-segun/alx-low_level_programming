9684938ff0c8cfb67385e31381564430585eae96
Segun Aremu <segun.aremu@outlook.com>
2023-10-06 17:38:08 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 
 /**
- *main -> This assigns a random number to the variable 
- *n each time it is executed and prints 
- *the last digit of the number stored in the variable n.
- *Return: Always 0 (Success)
- */
+*main -> This assigns a random number to the variable
+*n each time it is executed and prints
+*the last digit of the number stored in the variable n.
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n;
