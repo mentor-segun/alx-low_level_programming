539edbf09c5c7877fcd2a3463b413a8685180487
Segun Aremu <segun.aremu@outlook.com>
2023-10-10 15:48:01 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * This program prints the alphabet
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	char q;
+
+	for (q = 'a'; q <= 'z'; q++)
+	{
+		_putchar(q);
+	}
+
+	_putchar('\n');
+}
