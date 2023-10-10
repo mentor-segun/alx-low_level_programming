1bcdd77f7a6aa942909264bc611e23a2cd9d053f
Segun Aremu <segun.aremu@outlook.com>
2023-10-10 16:25:41 -0700

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the lowercase alphabet followed by a new line.
+ * main - check the code
  *
- * Return: void
+ * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
-    char letter = 'a';
-
-    while (letter <= 'z')
-    {
-        _putchar(letter);
-        letter++;
-    }
-
-    _putchar('\n');
+    print_alphabet();
+    return (0);
 }
