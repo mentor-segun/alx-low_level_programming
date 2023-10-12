1827991130ae017ab6f54f82d7fb744cb9caf291
Segun Aremu <segun.aremu@outlook.com>
2023-10-11 17:13:43 -0700

    Print Last Digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - This function prints the last digit of a given number
+ * print_last_digit - prints the last digit of a number
  *
  * @n: The number whose last digit needs to be printed.
  *
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_times_table(int n);
+int print_last_digit(int n);
 
 #endif /* MAIN_H */
