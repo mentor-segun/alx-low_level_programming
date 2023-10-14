f0b0a39089dcbaba12f3adcf93976e7f1c6a0cbb
Segun Aremu <segun.aremu@outlook.com>
2023-10-14 16:25:50 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10) // Comment out the while loop to avoid the infinite loop
+	while (i < 10)
+		/** Comment out the while loop to avoid the infinite loop */
 	{
-		// putchar(i);
+		/** putchar(i); */
 	}
 	printf("Infinite loop avoided! \\o/\n");
 
