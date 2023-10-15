a7c72eb44af092444fb854b60dd0ca1f988d6330
Segun Aremu <segun.aremu@outlook.com>
2023-10-15 02:53:00 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -13,6 +13,6 @@ int main(void)
 	c = 'A';
 	printf("%c: %d\n", c, _isupper(c));
 	c = 'a';
-	printf("%c: %d\n", c, _isupper(c);
-			return (0);
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
