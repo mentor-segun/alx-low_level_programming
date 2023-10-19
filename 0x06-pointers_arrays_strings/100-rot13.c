de45d7794731bf941ed23fb3cf13e9133c435b9b
Segun Aremu <segun.aremu@outlook.com>
2023-10-19 16:47:35 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rot13 - Encode a string using ROT13.
+ * @str: The input string.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			char base = (str[i] >= 'a') ? 'a' : 'A';
+			str[i] = base + ((str[i] - base + 13) % 26);
+		}
+	}
+
+	return (str);
+}
