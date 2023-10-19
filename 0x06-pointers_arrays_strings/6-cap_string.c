32b19f24ca3e3cda95db1b5dfa6dce0c78d2b656
Segun Aremu <segun.aremu@outlook.com>
2023-10-19 15:38:31 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,51 +1,53 @@
 #include "main.h"
 
 /**
- * is_separator - Check if a character is a word separator.
- * @c: The character to check.
- * Return: 1 if it's a separator, 0 if not.
+ * is_separator - Function checks if character is a word separator
+ * @c: The character to be checked
+ * Return: 1 if separator, 0 if not.
  */
 int is_separator(char c)
 {
-    char separators[] = " \t\n,;.!?\"(){}";
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
 
-    for (int i = 0; separators[i] != '\0'; i++)
-    {
-        if (c == separators[i])
-        {
-            return 1; // It's a separator.
-        }
-    }
+	for (i = 0; separators[i] != '\0'; i++)
+	{
+		if (c == separators[i])
+		{
+			return (1);
+		}
+	}
 
-    return 0; // It's not a separator.
+	return (0);
 }
 
 /**
- * cap_string - Capitalize the first letter of each word in a string.
- * @str: The input string.
+ * cap_string - Function capitalizes first letter of strings
+ * @str: The input string
  *
  * Return: A pointer to the modified string.
  */
 char *cap_string(char *str)
 {
-    int capitalize_next = 1; // Flag to indicate if the next character should be capitalized.
+	int capitalize_next = 1;
+	int i;
 
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-        if (capitalize_next && is_separator(str[i])) // Check for a separator.
-        {
-            capitalize_next = 1;
-        }
-        else if (capitalize_next && str[i] >= 'a' && str[i] <= 'z') // Check for lowercase letter.
-        {
-            str[i] = str[i] - 32; // Convert to uppercase.
-            capitalize_next = 0;
-        }
-        else
-        {
-            capitalize_next = 0;
-        }
-    }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (capitalize_next && is_separator(str[i]))
+		{
+			capitalize_next = 1;
+		}
+		else if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i] - 32;
+		capitalize_next = 0;
+	}
+		else
+		{
+			capitalize_next = 0;
+		}
+	}
 
-    return str;
+	return (str);
 }
