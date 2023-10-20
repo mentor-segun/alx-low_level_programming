062562844986d972e51c83d036cd7a5cff5c9d0f
Segun Aremu <segun.aremu@outlook.com>
2023-10-19 17:43:32 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,50 +1,66 @@
 #include "main.h"
 
 /**
- * infinite_add - Add two numbers represented as strings.
- * @n1: The first number.
- * @n2: The second number.
+ * infinite_add - Adds two numbers stored as strings.
+ * @n1: The first number as a string.
+ * @n2: The second number as a string.
  * @r: The buffer to store the result.
- * @size_r: The size of the buffer.
+ * @size_r: The size of the buffer r.
  *
- * Return: A pointer to the result, or 0 if the result does not fit in the buffer.
+ * Return: A pointer to the result or 0 if the result cannot be stored in r.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0;
+    int len1, len2, carry = 0;
+    int i, j, k;
 
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
+    for (len1 = 0; n1[len1] != '\0'; len1++)
+        ;
+    for (len2 = 0; n2[len2] != '\0'; len2++)
+        ;
 
-	if (len1 > size_r || len2 > size_r)
-		return (0);
+    if (len1 + 2 > size_r || len2 + 2 > size_r)
+        return (0);
 
-	r[size_r] = '\0';
-	len1--;
-	len2--;
+    i = len1 - 1;
+    j = len2 - 1;
+    k = 0;
 
-	for (int i = size_r - 1; len1 >= 0 || len2 >= 0 || carry; i--)
-	{
-		int sum = carry;
+    while (i >= 0 || j >= 0 || carry)
+    {
+        int num1 = (i >= 0) ? n1[i] - '0' : 0;
+        int num2 = (j >= 0) ? n2[j] - '0' : 0;
+        int sum = num1 + num2 + carry;
 
-		if (len1 >= 0)
-			sum += n1[len1] - '0';
-		if (len2 >= 0)
-			sum += n2[len2] - '0';
+        if (sum > 9)
+        {
+            carry = 1;
+            sum -= 10;
+        }
+        else
+        {
+            carry = 0;
+        }
 
-		carry = sum / 10;
-		r[i] = (sum % 10) + '0';
+        r[k++] = sum + '0';
 
-		if (len1 >= 0)
-			len1--;
-		if (len2 >= 0)
-			len2--;
-	}
+        if (i >= 0)
+            i--;
+        if (j >= 0)
+            j--;
 
-	if (r[0] == '0')
-		return r + 1;
-	else
-		return r;
+        if (k >= size_r)
+            return (0);
+    }
+
+    r[k] = '\0';
+
+    for (i = 0, j = k - 1; i < j; i++, j--)
+    {
+        char temp = r[i];
+        r[i] = r[j];
+        r[j] = temp;
+    }
+
+    return r;
 }
