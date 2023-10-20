fc1203653e26ac098263e3a97854892296bb166d
Segun Aremu <segun.aremu@outlook.com>
2023-10-19 18:15:14 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/10333-infinite_add.c
+++ 0x06-pointers_arrays_strings/10333-infinite_add.c
@@ -10,34 +10,44 @@
  * Return: A pointer to the result
  * or 0 if the result cannot be stored in r.
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, i, j, k = 0, carry = 0;
+	int i, j, k, len1, len2, carry = 0;
 
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
-
+	for (len1 = 0; n1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; n2[len2] != '\0'; len2++)
+		;
 	if (len1 + 2 > size_r || len2 + 2 > size_r)
 		return (0);
+	i = len1 - 1;
+	j = len2 - 1;
+	k = 0;
 
-	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--, k++)
+	while (i >= 0 || j >= 0 || carry)
 	{
 		int num1 = (i >= 0) ? n1[i] - '0' : 0;
 		int num2 = (j >= 0) ? n2[j] - '0' : 0;
 		int sum = num1 + num2 + carry;
 
-		carry = sum / 10;
-		r[k] = sum % 10 + '0';
-
+		if (sum > 9)
+		{
+			carry = 1;
+			sum -= 10;
+		}
+		else
+		{
+			carry = 0;
+		}
+		r[k++] = sum + '0';
+		if (i >= 0)
+			i--;
+		if (j >= 0)
+			j--;
 		if (k >= size_r)
 			return (0);
 	}
-
 	r[k] = '\0';
-
 	for (i = 0, j = k - 1; i < j; i++, j--)
 	{
 		char temp = r[i];
@@ -45,6 +55,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		r[i] = r[j];
 		r[j] = temp;
 	}
-
 	return (r);
 }
