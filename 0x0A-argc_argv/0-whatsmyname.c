c41552a0f9ac015d7d43d5f6242fffec4e788e3c
Segun Aremu <segun.aremu@outlook.com>
2023-10-27 21:48:19 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of strings containing the arguments
+ * 
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[]) 
+{
+	/* Check if there's at least one argument */
+	if (argc > 0)
+	{
+		/* Print the name of the program taken from argv[0] */
+		printf("%s\n", argv[0]);
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
