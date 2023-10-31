be9f1e53703a86698eb3589526f0e9e0cfcf5dfb
Segun Aremu <segun.aremu@outlook.com>
2023-10-31 12:04:07 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -5,6 +5,6 @@ int main(int argc, char *argv[]);
 int main(int argc, __attribute__((unused)) char *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
-int is_positive_number(char *s)
+int is_positive_number(char *s);
 
 #endif
