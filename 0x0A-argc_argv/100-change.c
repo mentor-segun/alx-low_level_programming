e381e4e9dc70653b04ca1b68c869283689df816b
Segun Aremu <segun.aremu@outlook.com>
2023-10-31 12:14:27 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,32 +4,27 @@
 /**
  * main - Function prints the minimum number of coins
  *		to make change for an amount of money
- * @argc - Number
- * @argv - Pointer
+ * @argc:  Number of arguments
+ * @argv: Array of strings containing arguments
  *
  * Return: Number of coins or 0 if the number is negative
  */
-
 int main(int argc, char *argv[])
 {
-	int cents;
-	int coins;
+	int cents, coins;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	cents = atoi(argv[1]);
 	coins = 0;
-
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-
 	while (cents > 0)
 	{
 		if (cents >= 25)
