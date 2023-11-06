287c39d7d37717d2a1aa875ca08ba13b71f1f1f8
Segun Aremu <segun.aremu@outlook.com>
2023-11-06 11:26:31 -0800

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure representing a dog
+ *
+ * @name: name element of char type
+ * @age: age element of float type
+ * @owner: owner element of char type
+ *
+ */
 struct dog
 {
 	char *name;
